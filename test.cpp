e3fce8b4bbd9b83facdff7dfba7f3d6340b5b71b
qxforever <53657439+qxforever@users.noreply.github.com>
2020-11-06 11:26:39 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -13,29 +13,28 @@ string s;
 int cnt;
 
 map<char, int> f = map<char, int>{
-	{'+', 2}, {'*', 4}, {'(', 0}, {')', 6}, {'i', 6}, {'#', 0}
+	{'+', 2}, {'*', 4}, {'(', 0}, {')', 6}, {'i', 6}, {'#', 0}, {'N', -1}
 };
 
 map<char, int> g = map<char, int>{
-	{'+', 1}, {'*', 3}, {'(', 5}, {')', 0}, {'i', 5}, {'#', 0}
+	{'+', 2}, {'*', 3}, {'(', 5}, {')', 0}, {'i', 5}, {'#', 0}
 };
 
-bool compare(const char & lhs, const char & rhs) {
+bool compare(const char &lhs, const char &rhs) {
 	return f[lhs]<g[rhs];
 }
 
-bool isoperator(char c){
-	return c == '+' || c == '*';
+bool canComp(const char &lhs, const char &rhs) {
+	return !(lhs == rhs);
 }
 
 bool issym(char c){
-	return c == '+' || c == '*' || c == '(' || c == ')' || c == '#';
+	return c == '+' || c == '*' || c == '(' || c == ')' || c == '#' || c == 'i';
 }
 
 void push(stack<char> &s, char c){
 	if (c == '#') return ;
-	if (isoperator(c)) cnt++;
-	if (isoperator(c) || c == '(' || c == 'i') s.push(c);
+	s.push(c);
 	cout << "I" << c << '\n';
 }
 
@@ -53,57 +52,56 @@ void E(){
 	exit(0);
 }
 
-void printstk(const stack<char> s){
-	// cout << "top" << s.top() << '\n';
+void work(stack<char> &s){
+	if (s.top() == ')'){
+		if (s.size() < 4) RE();
+		char ch[3];
+		for (int i = 0; i < 3; i++){
+			ch[i] = s.top();
+			s.pop(); 
+		}
+		if (!(ch[0] == ')') && ch[1] == 'N' && ch[2] == '(') RE();
+		s.push('N');
+		R();
+		return ;
+	}
+	if (s.top() != 'N' && s.top() != 'i') RE();
+	if (s.top() == 'i'){
+		s.pop();
+		s.push('N');
+		R();
+		return ;
+	}
+	else {
+		if (s.size() < 4) RE();
+		char ch[3];
+		for (int i = 0; i < 3; i++){
+			ch[i] = s.top();
+			s.pop(); 
+		}
+		if (!(ch[0] == 'N') && (ch[1] == '+' || ch[1] == '*') && ch[2] == 'N') RE();
+		R();
+		s.push('N');
+		return ;
+	}
 }
 
 int main() {
 	cin >> s;
 	s += '#';
-	stack<char> a, b;
-	b.push('#');
-	int n = s.length();
+	stack<char> a;
+	a.push('#');
+	for (size_t i = 0; i < s.length(); i++) {
+		if(!issym(s[i]) || !canComp(a.top(), s[i])) E();
 
-	for (int i = 0; i < n; i++) {
-		printstk(b);
-		if (issym(s[i])) {
-			if (compare(b.top(), s[i])) {
-				push(b, s[i]);
-			}
-			else {
-				while (!compare(b.top(), s[i])) {
-					// printf ("cmp : %c %c\n",b.top(), s[i]);
-					if (f[b.top()] == g[s[i]]){
-						b.pop();
-						break;
-					}
-					if (isoperator(b.top())) {
-						if (a.size() < 2){
-							RE();
-						}
-						a.pop();
-						cnt--;
-					}
-					else {
-						if (a.size() < 1) {
-							RE();
-						}
-						a.pop();
-					}
-					b.pop();
-					R();
-				}
-				push(b, s[i]);
-				if (s[i] == ')') R();
-			}
+		while(!compare(a.top(), s[i])){
+			// printf("%c %c %d\n", a.top(), s[i], compare(a.top(), s[i]));
+			work(a);
 		}
-		else if (s[i] == 'i') {
-			if (a.size() > cnt) E();
+		
+		if (compare(a.top(), s[i])) {
 			push(a, s[i]);
-			if(issym(s[i + 1])) R();
 		}
-		else E();
 	}
-	// cout << a.size() << " " << b.size() << '\n';
-	if(!(b.size() == 0 && a.size() == 1)) RE();
+	while (a.size() > 2) work(a);
 }
